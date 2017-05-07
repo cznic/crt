612bc7d81adaac82d0cbf65abe939d5111970835
Jan Mercl <0xjnml@gmail.com>
2017-05-07 23:26:40 +0200

    Add package comment.
    	modified:   etc.go

--- etc.go
+++ etc.go
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// Package crt provides C-runtime services. (Work In Progress)
 package crt
 
 import (
