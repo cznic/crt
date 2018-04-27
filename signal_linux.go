7762ce92d73e338bd33f4bc5017480a9cffa0725
Jan Mercl <0xjnml@gmail.com>
2018-04-27 16:30:38 +0200

    Overhaul signal.
    	modified:   signal_linux.go

--- signal_linux.go
+++ signal_linux.go
@@ -7,9 +7,35 @@ package crt
 import (
 	"os"
 	"os/signal"
+	"sync"
 	"syscall"
 	"unsafe"
 )
+
+// /* Fake signal functions.  */
+// #define SIG_ERR	((__sighandler_t) -1)	/* Error return.  */
+// #define SIG_DFL	((__sighandler_t) 0)	/* Default action.  */
+// #define SIG_IGN	((__sighandler_t) 1)	/* Ignore signal.  */
+//
+// #ifdef __USE_UNIX98
+// # define SIG_HOLD	((__sighandler_t) 2)	/* Add signal to hold mask.  */
+// #endif
+const (
+	sigErr  = -1
+	sigDfl  = 0
+	sigIgn  = 1
+	sigHold = 2
+)
+
+var (
+	sigMap   = map[int32]*sigHandler{}
+	sigMapMu sync.Mutex
+)
+
+type sigHandler struct {
+	ch      chan os.Signal
+	handler uintptr
+}
 
 // sighandler_t signal(int signum, sighandler_t handler);
 func Xsignal(tls TLS, signum int32, handler uintptr) uintptr {
@@ -17,12 +43,41 @@ func Xsignal(tls TLS, signum int32, handler uintptr) uintptr {
 }
 
 // sighandler_t sysv_signal(int signum, sighandler_t handler);
-func X__sysv_signal(tls TLS, signum int32, handler uintptr) uintptr {
-	ch := make(chan os.Signal)
-	go func() {
-		<-ch
-		(*(*func(TLS, int32))(unsafe.Pointer(&handler)))(tls, signum)
-	}()
-	signal.Notify(ch, syscall.Signal(signum))
-	return 0 //TODO previous handler
+func X__sysv_signal(tls TLS, signum int32, handler uintptr) (r uintptr) {
+	sigMapMu.Lock()
+
+	defer sigMapMu.Unlock()
+
+	switch long_t(handler) {
+	case sigErr:
+		panic("TODO")
+	case sigDfl:
+		panic("TODO")
+	case sigIgn:
+		if sh := sigMap[signum]; sh != nil {
+			sh.ch <- nil
+			r = sh.handler
+			delete(sigMap, signum)
+		}
+		signal.Ignore(syscall.Signal(signum))
+	case sigHold:
+		panic("TODO")
+	default:
+		if sh := sigMap[signum]; sh != nil {
+			sh.ch <- nil
+			r = sh.handler
+			delete(sigMap, signum)
+		}
+		ch := make(chan os.Signal)
+		sigMap[signum] = &sigHandler{ch, handler}
+		go func() {
+			if <-ch == nil {
+				return
+			}
+
+			(*(*func(TLS, int32))(unsafe.Pointer(&handler)))(tls, signum)
+		}()
+		signal.Notify(ch, syscall.Signal(signum))
+	}
+	return r
 }
